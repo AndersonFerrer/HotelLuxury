82c8006970feaf4e60f1d191d78725b9095ee980
AndersonFerrer <ferrer.280104@gmail.com>
2025-06-17 22:12:53 -0500

    feat(rooms): dynamically load and display available rooms
    Modify SQL query to fetch room types instead of individual rooms and implement client-side rendering of room cards. The changes include:
    - Simplifying the database query to get distinct room types
    - Adding JavaScript to dynamically create and display room cards
    - Removing hardcoded room HTML in favor of dynamic generation

--- php/obtenerHabitaciones.php
+++ php/obtenerHabitaciones.php
@@ -12,19 +12,12 @@ if (ob_get_length()) ob_clean();
 
 try {
     // OBTENER HABITACIONES DISPONIBLES
-    $sql = "SELECT 
-                h.id_habitacion,
-                h.numero,
-                th.nombre AS tipo,
-                th.precio_noche,
-                string_agg(ch.nombre, ', ') AS caracteristicas
-            FROM Habitacion h
-            JOIN TipoHabitacion th ON h.id_tipo_habitacion = th.id_tipo_habitacion
-            LEFT JOIN HabitacionCaracteristica hc ON h.id_habitacion = hc.id_habitacion
-            LEFT JOIN CaracteristicaHabitacion ch ON hc.id_caracteristica = ch.id_caracteristica
+    $sql = "SELECT DISTINCT th.id_tipo_habitacion, th.nombre, th.descripcion, th.precio_noche
+            FROM TipoHabitacion th
+            JOIN Habitacion h ON th.id_tipo_habitacion = h.id_tipo_habitacion
             WHERE h.estado = 'Disponible'
-            GROUP BY h.id_habitacion, th.nombre, th.precio_noche";
-    
+            ORDER BY th.precio_noche ASC
+            LIMIT 4;";
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     $habitaciones = $stmt->fetchAll();
